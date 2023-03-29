2bbc812c7a942d08b9d729d62e10fe281e7c789a
20221TIIMI0351 <evelynpo59@gmail.com>
2023-03-29 11:42:18 -0300

    Atividade 2 App Email comentada.

--- app/src/main/java/pereira/otavio/evelyn/email/MainActivity.java
+++ app/src/main/java/pereira/otavio/evelyn/email/MainActivity.java
@@ -1,5 +1,6 @@
 package pereira.otavio.evelyn.email;
 
+// Importação das classes necessárias
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
@@ -12,12 +13,15 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+// Definição da classe MainActivity que herda de AppCompatActivity
 public class MainActivity extends AppCompatActivity {
 
+    // Método onCreate que é chamado quando a atividade é criada
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Busca o botão com id btnEnviar no layout activity_main
         Button btnEnviar = (Button) findViewById(R.id.btnEnviar);
         //Definição da ação do click do botão
         btnEnviar.setOnClickListener(new View.OnClickListener() {
@@ -29,22 +33,31 @@ public class MainActivity extends AppCompatActivity {
                 EditText etEmail = (EditText) findViewById(R.id.etEmail);
                 String email = etEmail.getText().toString();
 
+                // Obtém o assunto digitado pelo usuário
                 EditText etAssunto = (EditText) findViewById(R.id.etAssunto);
                 String assunto = etAssunto.getText().toString();
 
+                // Obtém o texto digitado pelo usuário
                 EditText etTexto = (EditText) findViewById(R.id.etTexto);
                 String texto = etTexto.getText().toString();
 
+                // Cria uma nova intenção para enviar um e-mail
                 Intent i = new Intent(Intent.ACTION_SENDTO);
+                // Define o protocolo URI para enviar um e-mail
                 i.setData(Uri.parse("mailto:"));
+                // Define o endereço de e-mail para o qual o e-mail será enviado
                 String[] emails = new String[]{email};
                 i.putExtra(Intent.EXTRA_EMAIL,emails);
+                // Define o assunto do e-mail
                 i.putExtra(Intent.EXTRA_SUBJECT,assunto);
+                // Define o corpo do e-mail
                 i.putExtra(Intent.EXTRA_TEXT,texto);
+                // Tenta iniciar uma atividade para enviar o e-mail
                 try {
                     startActivity(Intent.createChooser(i,"Escolha o APP"));
                 }
                 catch(ActivityNotFoundException e) {
+                    // Se não houver nenhum aplicativo de e-mail instalado, exibe uma mensagem de erro
                     Toast.makeText(MainActivity.this, "Não há nenhum app que posso realizar essa operação",
                             Toast.LENGTH_LONG).show();
                 }
